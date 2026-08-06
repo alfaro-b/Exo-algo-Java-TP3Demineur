8d5ad2b5488237ad86e9772e69bb44ba12439b7e
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-08-06 11:10:26 +0200

    Ajout du placement aléatoire des bombes

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,19 +1,43 @@
+import java.util.Random;
 
 public class Minesweeper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[][] grid = createGrid(6, 12);
+		placeBombs(9, grid);
 	}
 	
 	/**
 	 *  Crée la grille de jeu
-	 *  @param row Nombre entier pour le nombre de lignes
-	 *  @param column Nombre entier pour le nombre de colonnes
+	 *  @param rows Nombre de lignes
+	 *  @param columns Nombre de colonnes
+	 *  @return La grille créée
 	 */
-	public static int [][] createGrid(int row, int column) {
-		int [][] grid = new int[row][column];
+	public static int [][] createGrid(int rows, int columns) {
+		int[][] grid = new int[rows][columns];
 		return grid;
+	}
+	
+	/**
+	 *  Place les bombes aléatoirement sur la grille de jeu
+	 *  @param bombsNumber Nombre de bombes à placer
+	 *  @param grid Grille de jeu
+	 */
+	public static void placeBombs(int bombsNumber, int[][] grid) {
+		int maxRow = grid.length;
+		int maxColumn = grid[0].length;
+		Random random = new Random();
+		
+		for (int i = 0; i<bombsNumber; i++) {
+			int randomNumRow = random.nextInt(maxRow);
+			int randomNumColumn = random.nextInt(maxColumn);
+			
+			while(grid[randomNumRow][randomNumColumn] == -1) {
+				randomNumRow = random.nextInt(maxRow);
+				randomNumColumn = random.nextInt(maxColumn);
+			}
+			grid[randomNumRow][randomNumColumn] = -1;
+		}
 	}
 
 }
